099ddb4c7d30430b6c250a2e0679610f3ed428b0
Milad <Milad@Milads-MacBook-Pro.local>
2016-12-08 00:43:48 -0800

    removed Pod files updated .gitignore

--- iOS/Pods/Target Support Files/Pods-Fraiche/Pods-Fraiche-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_FraicheVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_FraicheVersionString[];
-
--- iOS/Pods/Target Support Files/Pods-FraicheTests/Pods-FraicheTests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_FraicheTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_FraicheTestsVersionString[];
-
